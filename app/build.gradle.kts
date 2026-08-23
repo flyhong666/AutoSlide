3fa15741f0605e69c77b08866572c6a781970a56
flyhong666 <76857714+flyhong666@users.noreply.github.com>
2026-08-23 20:59:35 +0800

    Update versionName to 2.5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         // 版本号
         versionCode = 18
         // 版本名称
-        versionName = "2.5.5"
+        versionName = "2.5.6"
         // 单元测试
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
